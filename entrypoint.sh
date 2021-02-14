251ca83fd59a349bf3d591858cbc516e8245b79f
Marcello de Sales <marcello.desales@gmail.com>
2021-02-13 17:17:40 -0800

    :art: Just adding output before generating

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,15 @@ JACOCO_XML=${JACOCO_XML:-"JACOCO_XML env var missing: file path in a volume!"}
 SRC_MAIN_JAVA_DIR=${SRC_MAIN_JAVA_DIR:-"SRC_MAIN_JAVA_DIR env var missing: the source dir!"}
 COBERTURA_XML_FILE=${COBERTURA_XML_FILE:-"COBERTURA_XML_FILE env var missing: file path to cobertura xml file!"}
 
+echo "Translating JaCoco '${JACOCO_XML}' to Cobertura '${COBERTURA_XML_FILE}'"
 /tool/cover2cover.py ${JACOCO_XML} ${SRC_MAIN_JAVA_DIR} > ${COBERTURA_XML_FILE}
 
-if [ ! -z ${DEBUG} ]; then
+if [ ! -z ${PRINT_REPORT} ]; then
   if [ -f ${COBERTURA_XML_FILE} ]; then
     echo "Generated cobertura xml at '${COBERTURA_XML_FILE}'"
     ls -la ${COBERTURA_XML_FILE}
     cat ${COBERTURA_XML_FILE}
+
   else
     echo "ERROR: failed to generate file at '${COBERTURA_XML_FILE}'"
   fi
